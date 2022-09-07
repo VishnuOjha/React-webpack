46511f66cb5f830207bc5f2ebfde8cbf2c723970
Vishnu Ojha <vishnu.o@heyarrow.com>
2022-09-07 23:07:22 +0530

    adding webpack with react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
 const path = require("path");
 
 module.exports = {
-    
-}
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    port: 3010,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
